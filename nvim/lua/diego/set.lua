336bf2e359fa3e10b4ac0f1043c7b7683c4c655d
Diego Araujo <diegolaraujo96@gmail.com>
2023-09-19 23:04:23 -0300

    Update packages

--- nvim/lua/diego/set.lua
+++ nvim/lua/diego/set.lua
@@ -1,16 +1,17 @@
+local tablength = 4
+vim.opt.tabstop = tablength
+vim.opt.softtabstop = tablength
+vim.opt.shiftwidth = tablength
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.smartindent = true
 vim.opt.wrap = true
 vim.opt.ignorecase = true
-
 vim.opt.completeopt = "menu,noinsert,noselect"
 
 vim.g.mapleader = " "
